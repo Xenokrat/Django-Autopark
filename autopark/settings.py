8309235ba70daf8931a9f66805193dd493840865
Mantis <barabanovpv95@gmail.com>
2023-01-21 17:43:56 +0300

    feat: add rest framework

--- autopark/settings.py
+++ autopark/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # third party
+    "rest_framework",
     # myapps
     "auto.apps.AutoConfig",
 ]
